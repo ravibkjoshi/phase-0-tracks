1e0774377443cbd745c3ea6a5894d01bdf96d01f
ravibkjoshi <ravibkjoshi@gmail.com>
2016-12-02 17:55:45 -0800

    Add to do list and require list spec

--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,5 +1,8 @@
+require_relative "todo_list"
+
 describe TodoList do
   let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
+
 
   it "stores the list items given on initialization" do
     expect(list.get_items).to eq ["do the dishes", "mow the lawn"]
