61d526cda6b74acd0c560214766bfeeb460cbe05
ravibkjoshi <ravibkjoshi@gmail.com>
2017-01-16 04:05:22 -0800

    add user functionality and db

--- databases/8.5_challenge/week8_challenge.rb
+++ databases/8.5_challenge/week8_challenge.rb
@@ -6,6 +6,7 @@ require 'faker'
 
 # create SQLite3 database
 db = SQLite3::Database.new("students.db")
+db.results_as_hash = true
 
 
 # learn about fancy string delimiters
@@ -27,10 +28,15 @@ end
 def remove_student(db, name)
   db.execute("DELETE FROM students WHERE name = ?", [name])  
 end
-def update_student(db,address,name)
+def update_student(db,name,address)
 	db.execute("UPDATE students SET address= #{address} WHERE name=?" ,[name]) 
-  
 end
+def view_students(db)
+	 list = db.execute("SELECT * FROM students")
+  	 list.each do |names,address|
+    puts "#{names} : #{address}"
+  end
+end 
 
 puts " Welcome to student records!"
 puts ""
@@ -43,22 +49,31 @@ end
 
 done = false
 until done
-puts "Please select what you would like to do with our records(add,remove,update,view, or done if finished)"
-response = gets.chomp
-case response
-when "add"
-  puts " You have selected to add a student. What is this new student's name?"
-  	new_student_name = gets.chomp
-  	puts " What is #{new_student_name}'s address?"
-  	new_student_address = gets.chomp
-  	create_student(db, new_student_name, new_student_address)
-when "remove"
-  puts "You have selected to remove a student from our records. Which student would you like to remove?"
-  	removed_student = gets.chomp
-when "update"
-  puts 'You need help!!!'
-when "view"
-  puts "You just making it up!"
+	puts "Please select what you would like to do with our records(add,remove,update,view, or done if finished)"
+	response = gets.chomp
+	case response
+    when "add"
+  		puts " You have selected to add a student. What is this new student's name?"
+  		new_student_name = gets.chomp
+  		puts " What is #{new_student_name}'s address?"
+  		new_student_address = gets.chomp
+  		create_student(db, new_student_name, new_student_address)
+	when "remove"
+  		puts "You have selected to remove a student from our records. Which student would you like to remove?"
+  		removed_student = gets.chomp
+  		remove_student(db,removed_student)
+	when "update"
+  		puts "You have selected to remove a student from our records. Which student would you like to update?"
+  		updated_student = gets.chomp
+  		puts "What is there new address?"
+  		updated_address = gets.chomp
+  		update_student(db,updated_student,updated_address)
+	when "view"
+  		view_students(db)
+  	when "done"
+  	puts "Thank you for using student records! Have a great day!"
+  		done = true
+end
 end
 
 
